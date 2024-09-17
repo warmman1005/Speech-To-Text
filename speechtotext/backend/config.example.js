574836182ea225bbe35e3e6246a95b73d2e857b3
warmman1005 <154582206+warmman1005@users.noreply.github.com>
2024-09-18 01:20:42 +0800

    Delete speechtotext directory

--- speechtotext/backend/config.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  apiKeyGoogle: 'YOUR_GOOGLE_API_KEY',
-  openAIKey: 'YOUR_OPENAI_API_KEY'
-};
--- speechtotext/backend/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    apiKeyGoogle: '實際的Google API密鑰',
-    openAIKey: '實際的OpenAI API密鑰'
-  };
-  
